e182465f0e48727198bcdd46f356fa2eee5d4624
Ivan Chavero <ichavero@redhat.com>
2020-07-06 17:35:48 -0500

    Disable debug

--- src/sm/sm/settings.py
+++ src/sm/sm/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'n0mhw$41jdwu3&4^xuz0hqwf2=rf546qoh3h_cg*#c^hhg2eqp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
