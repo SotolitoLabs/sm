bdd75f7a5bea5fa99ea5507626dbedcd9b3c5751
Iván Chavero <ichavero@chavero.com.mx>
2020-07-18 20:04:05 -0500

    Update settings.py

--- src/sm/sm/settings.py
+++ src/sm/sm/settings.py
@@ -122,6 +122,13 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'sm_tests.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
