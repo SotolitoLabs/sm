dd4a2417b836c0df30cf4fca3df5d711003c4f0e
Ivan Chavero <ichavero@redhat.com>
2020-07-19 13:07:06 -0500

    Initial models.py commit

--- src/sm/sm_tests/models.py
+++ src/sm/sm_tests/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+class MentalTest(models.Model):
+    owner = models.ForeignKey('auth.User', on_delete=models.DO_NOTHING)
+    name  = models.CharField(max_length=255)
+    description =  models.TextField(blank=True, null=True)
+    
+    def __str__(self):
+        return self.name
+
+class MentalTestFieldType(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class MentalTestField(models.Model):
+    test = models.ForeignKey(MentalTest, on_delete=models.DO_NOTHING)
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    field_type = models.ForeignKey(MentalTestFieldType, related_name='fieldtypes', on_delete=models.DO_NOTHING)
+    weight = models.IntegerField(default=1)
+
+    def __str__(self):
+        return self.name
+
+class MentalTestResult(models.Model):
+    test = models.ForeignKey(MentalTest, on_delete=models.DO_NOTHING)
+    user = models.ForeignKey('auth.User', related_name='mentaltests', on_delete=models.DO_NOTHING)
+    test_field = models.ForeignKey(MentalTestField, related_name='mentaltestfieldtypes', on_delete=models.DO_NOTHING)
+    value = models.CharField(max_length=5)
+
+    def __str__(self):
+        return self.test_field.name + " :: " + self.user.name
+
