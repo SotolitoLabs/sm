aa8e9b1efeef35df41bac93ed46bab764279dd44
Ivan Chavero <ichavero@redhat.com>
2020-07-19 13:07:18 -0500

    Initial view.py commit

--- src/sm/sm_tests/views.py
+++ src/sm/sm_tests/views.py
@@ -1,3 +1,52 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from .models import (MentalTest, MentalTestField, MentalTestFieldType,
+    MentalTestResult)
+from rest_framework import viewsets
+from .serializers import  (UserSerializer, MentalTestSerializer,
+        MentalTestFieldSerializer, MentalTestFieldTypeSerializer,
+        MentalTestResultSerializer)
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint for users
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+class MentalTestViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint for users
+    """
+    queryset = MentalTest.objects.all().order_by('-id')
+    serializer_class = MentalTestSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+class MentalTestFieldViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint for users
+    """
+    queryset = MentalTestField.objects.all().order_by('-id')
+    serializer_class = MentalTestFieldSerializer
+
+class MentalTestFieldTypeViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint for users
+    """
+    queryset = MentalTestFieldType.objects.all().order_by('-id')
+    serializer_class = MentalTestFieldTypeSerializer
+
+class MentalTestResultViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint for users
+    """
+    queryset = MentalTestResult.objects.all().order_by('-id')
+    serializer_class = MentalTestResultSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+
