a5f42ca44714a2ed181e6f0abded4b0d1d2076a1
Ivan Chavero <ichavero@redhat.com>
2020-07-23 19:54:59 -0500

    Update run uwsgi script

--- src/run_uwsgi.sh
+++ src/run_uwsgi.sh
@@ -5,19 +5,33 @@
 echo "Collect static files"
 /code/sm/manage.py collectstatic
 
-echo "Ensure the development sqllite database is writable"
-chown 1000:2000 /code/sm/db.sqlite3
-chgrp 2000 /code/sm
-chmod g+rw /code/sm
 
-echo "Running production sm application"
-uwsgi --chdir=/code/sm \
-    --module=sm.wsgi:application \
-    --master --pidfile=/tmp/sm-master.pid \
-    --socket=127.0.0.1:8000 \
-    --processes=2 \
-    --uid=1000 --gid=2000 \
-    --harakiri=20 \
-    --max-requests=5000 \
-    --touch-reload=/code/reload.me \
-    --vacuum
+if [[ $UID != 0 ]]; then
+    echo "Ensure the development sqllite database is writable"
+    chown 1000:2000 /code/sm/db.sqlite3
+    chgrp 2000 /code/sm
+    chmod g+rw /code/sm
+    echo "Running production sm application"
+    uwsgi --chdir=/code/sm \
+        --module=sm.wsgi:application \
+        --master --pidfile=/tmp/sm-master.pid \
+        --socket=127.0.0.1:8000 \
+        --processes=2 \
+        --uid=1000 --gid=2000 \
+        --harakiri=20 \
+        --max-requests=5000 \
+        --touch-reload=/code/reload.me \
+        --vacuum
+else
+    echo "Running production sm application as allmighty root"
+    uwsgi --chdir=/code/sm \
+        --module=sm.wsgi:application \
+        --master --pidfile=/tmp/sm-master.pid \
+        --socket=127.0.0.1:8000 \
+        --processes=2 \
+        --harakiri=20 \
+        --max-requests=5000 \
+        --touch-reload=/code/reload.me \
+        --vacuum
+
+fi
