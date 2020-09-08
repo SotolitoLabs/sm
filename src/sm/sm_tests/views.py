9e83e04736bd7a4db2d05144bf5d9466a7307939
Ivan Chavero <ichavero@redhat.com>
2020-09-07 22:18:07 -0600

    Add multiple mental test create and update viewset

--- src/sm/sm_tests/views.py
+++ src/sm/sm_tests/views.py
@@ -1,16 +1,24 @@
+from rest_framework import viewsets, permissions
+from rest_framework.response import Response
+from rest_framework import status
+
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.contrib.auth.hashers import make_password
-from rest_framework import viewsets, permissions
+
 from .permissions import IsOwnerOrReadOnly
 from .models import (MentalTest, MentalTestField, MentalTestFieldType,
                      MentalTestResult)
 from .serializers import (UserSerializer, MentalTestSerializer,
                           MentalTestFieldSerializer, MentalTestFieldTypeSerializer,
-                          MentalTestResultSerializer)
+                          MentalTestResultSerializer, MentalTestResultCreateSerializer)
 from .renderers import PlainTextRenderer
 from rest_framework.renderers import (BrowsableAPIRenderer, JSONRenderer, AdminRenderer,
                                      TemplateHTMLRenderer)
+
+# Temporal imports
+import sys
+
 
 class UserViewSet(viewsets.ModelViewSet):
     """
@@ -41,6 +49,9 @@ class MentalTestViewSet(viewsets.ModelViewSet):
     #def get(self, request, pk):
     #    mt = get_object_or_404(MentalTest, pk=pk)
     #    serializer = MentalTestSerializer(mt)
+    #    import logging
+    #    logger = logging.getLogger(__name__)
+    #    logger.info("USER: " + user)
     #    return Response({'serializer': serializer})
 
     def perform_create(self, serializer):
@@ -74,4 +85,41 @@ class MentalTestResultViewSet(viewsets.ModelViewSet):
         serializer.save(owner=self.request.user)
 
 
+class MentalTestResultsViewSet(viewsets.ModelViewSet):
+    queryset = MentalTestResult.objects.all().order_by('-id')
+    serializer_class = MentalTestResultSerializer
+    user = ""
+
+
+    def create(self, request, *args, **kwargs):
+        instance = None
+        return_message = {"failed": "Failed to create Mental Test Result"}
+        return_status = status.HTTP_500_INTERNAL_SERVER_ERROR
+        write_serializer = MentalTestResultCreateSerializer(data=request.data, many=True, context={'request': request})
+        write_serializer.is_valid(raise_exception=True)
+
+        try:
+            self.perform_update(write_serializer)
+            return_message = {"success": "Mental Test Result Succesfully Updated"}
+            return_status = status.HTTP_200_OK
+        except:
+            instance = self.perform_create(write_serializer)
+            return_message = {"success": "Mental Test Result Succesfully Created"}
+            return_status = status.HTTP_201_CREATED
+
+        return Response(return_message, return_status)
+
+
+    def perform_create(self, serializer):
+        return serializer.save()
+
+
+    def perform_update(self, serializer):
+        for d in serializer.validated_data:
+            m = MentalTestResult.objects.get(test = d['test'], user = d['user'], test_field = d['test_field'] )
+            if not isinstance(m, list):
+                m.value = d['value']
+                m.save()
+            else:
+                raise Exception("Multiple response values")
 
