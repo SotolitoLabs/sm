8e1e19eba35fb806b2db73db16a33dcc1b6c12c1
Ivan Chavero <ichavero@redhat.com>
2020-09-22 01:15:02 -0600

    Add retrieve mental test result for user

--- src/sm/sm_tests/views.py
+++ src/sm/sm_tests/views.py
@@ -16,6 +16,9 @@ from .renderers import PlainTextRenderer
 from rest_framework.renderers import (BrowsableAPIRenderer, JSONRenderer, AdminRenderer,
                                      TemplateHTMLRenderer)
 
+
+from rest_framework.request import Request
+
 # Temporal imports
 import sys
 
@@ -41,18 +44,7 @@ class MentalTestViewSet(viewsets.ModelViewSet):
     """
     queryset = MentalTest.objects.all().order_by('-id')
     serializer_class = MentalTestSerializer
-    #renderer_classes = [TemplateHTMLRenderer, JSONRenderer]
-    #renderer_classes = [JSONRenderer]
     permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
-    #template_name = "test.html"
-
-    #def get(self, request, pk):
-    #    mt = get_object_or_404(MentalTest, pk=pk)
-    #    serializer = MentalTestSerializer(mt)
-    #    import logging
-    #    logger = logging.getLogger(__name__)
-    #    logger.info("USER: " + user)
-    #    return Response({'serializer': serializer})
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
@@ -84,11 +76,16 @@ class MentalTestResultViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
-
 class MentalTestResultsViewSet(viewsets.ModelViewSet):
     queryset = MentalTestResult.objects.all().order_by('-id')
     serializer_class = MentalTestResultSerializer
+    lookup_field = "test"
     user = ""
+
+    def retrieve(self, request, *args, **kwargs):
+        queryset = MentalTestResult.objects.filter(user = self.request.user.id, test = kwargs['test']).order_by('id')
+        serializer = MentalTestResultSerializer(queryset, many=True, context={'request': request})
+        return Response(serializer.data)
 
 
     def create(self, request, *args, **kwargs):
