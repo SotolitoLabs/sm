a11491203a1e52e6d176ce481f844c6dd71a35e6
Ivan Chavero <ichavero@chavero.com.mx>
2020-11-23 09:33:09 -0500

    Add initial token authentication javascript functions

--- src/sm/static/js/adapp/auth.js
+++ src/sm/static/js/adapp/auth.js
@@ -1,4 +1,18 @@
 
+// Our auth token
+var token = Cookies.get("token");
+alert("AUTH.JS TOKEN: " + token);
+
+function login(username, password) {
+  get_token(username, password);
+}
+
+
+function set_auth_info(token) {
+  Cookies.set('token', token);
+}
+
+
 function get_token(user, password) {
   console.log("Getting Token");
   $.post(api_host + "/token-auth/",
@@ -8,11 +22,14 @@ function get_token(user, password) {
   })
   .fail(function(jqxhr, text, error) {
     console.log("Login failed: " + jqxhr.responseText);
-    alert("Login failed: " + jqxhr.responseText);
+    alert("Login failed: " + jqxhr.responseText + " User:" + user + " pass: " + password);
   })
   .done(function(data) {
-    alert(data.token);
+    console.log("DATA: " + JSON.stringify(data));
+    set_auth_info(data.token);
+    location.href = "/mental_test.html";
   });
 }
+
 
 //https://www.django-rest-framework.org/topics/ajax-csrf-cors/
